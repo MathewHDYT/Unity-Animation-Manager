f2746cf4eab4c7ea6af1d29e3266f27da037696b
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2021-10-28 07:28:16 +0200

    Update Methodcalls to use AnimationError Enum
    Updated the PlayAnimation Call to use the AnimationError Enum and print an error depeding on its result.

--- Example Project/Assets/Scripts/MethodCalls.cs
+++ Example Project/Assets/Scripts/MethodCalls.cs
@@ -17,11 +17,29 @@ public class MethodCalls : MonoBehaviour {
     }
 
     public void PlayAnimationClicked() {
-        am.PlayAnimation(animator, inputField.text);
+        AninmationManager.AninmationError error = am.PlayAnimation(animator, inputField.text);
+        output.text = GetStringFromError(error);
     }
 
     public void GetCurrentAnimationLengthClicked() {
         float length = am.GetCurrentAnimationLength(animator);
         output.text = "Length of the currently playing animation in seconds: " + length;
+    }
+
+    private string GetStringFromError(AninmationManager.AnimationError error) {
+        switch (error) {
+            case AnimationError.Id.AlreadyPlaying:
+                // No Message
+                return;
+            case AnimationError.Id.DoesNotExist:
+                warning = "Given Animation Name does not exist  on the given Animator.";
+                break;
+            case AnimationError.Id.Success:
+                // No Message
+                return;
+            default:
+                warning = "Given AnimatonError State not defined.";
+                break;
+        }
     }
 }
